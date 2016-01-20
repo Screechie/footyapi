d4f2926651c5c5de677ad777c2f77b6e2033d78d
sim <simeonbourne@gmail.com>
2016-01-19 21:09:08 -0400

    Initial Commit

---
+++ app.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+
+		$(".form").submit(function (event){
+			event.preventDefault();
+			 var roster = $("#team").getval();
+
+
+
+
+
+
+		})
+
+
+		$(".form2").submit(function (event){
+			event.preventDefault();
+
+
+
+
+
+
+
+		})
+
+
+
+
+
+
+
+
+});
