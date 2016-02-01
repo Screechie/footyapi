5aed43c41f9ddf3de45958d59a0f22e204c2d8e5
sim <simeonbourne@gmail.com>
2016-01-31 23:33:46 -0400

    Change API Source

--- app.js
+++ app.js
@@ -1,71 +1,123 @@
 //$()- shorthand for $(document).ready()
 $(function(){
 
-	
-	var player = $('#player').val();//Get Player
+	//Set Autocompletion for teams
+		$("#roster").autocomplete({
+			source: function(request,response){
+				$.ajax({
+					headers:{"X-Mashape-Key": "bilVMebejrmshNXfDlOtYvNUOxjAp1pY0GEjsn7LV5xmaJ8Dwq"},
+					url: "https://sportsop-soccer-sports-open-data-v1.p.mashape.com/v1/leagues/serie-a/seasons/15-16/teams",
+					dataType: "json",
+					type: "GET"
+				}).done(function(data){
+					response.data;
+				});
+			} 
+		});
 
+	//Table to list a team's available players
 	$("#form").on("submit",function(event){
+
 		event.preventDefault();
 
-		var team = $('#roster').val();//Get Team		
-		
+		var team = $('#roster').val();//Get Team	
+
 		if(team == ""){
 			alert("Please enter a team name!");//No empty team field
 		}
 		else{
-			//return team squad	
-
+			$('#table3').dataTable({
+				//return team squad
+				"ajax" :{
+					"headers": {"X-Mashape-Key": "bilVMebejrmshNXfDlOtYvNUOxjAp1pY0GEjsn7LV5xmaJ8Dwq"},
+				 	"url": "https://sportsop-soccer-sports-open-data-v1.p.mashape.com/v1/leagues/serie-a/seasons/15-16/teams/"+team+"/players",
+				 	"dataSrc": "data.players",
+				 	"type": "GET",
+				},
+				"columns" :[
+					{"data" :"team"},
+					{"data" :"name"},
+				 	{"data" :"fullname" },
+				 	{"data" :"nationality" },
+				 	{"data" : "position"},
+				 	{"data" : "number"}
+				]
+			});	
+			
 		}	
 	});//End Form
 
-
+	//Table to get individual player profile
 	$("#form1").on("submit",function(event){
 		event.preventDefault();
-		
 
-		if(player == ""){
-			alert("Please enter a player's name!");
+		var player = $('#player').val();//Get Player
+		var team = $('#roster').val();//Get Team
+	
+		if(player == "" || team == ""){
+			alert("To get player stats both a name and team must be entered!");
 		}
-		else{
-		//return player profile
-		console.log(player);
+		else{		
+			//return player profile
+			console.log(player);
+			//get list of all available players
+			$.ajax({
+				 		headers:{"X-Mashape-Key": "bilVMebejrmshNXfDlOtYvNUOxjAp1pY0GEjsn7LV5xmaJ8Dwq"}, 
+				 		url: "https://sportsop-soccer-sports-open-data-v1.p.mashape.com/v1/leagues/serie-a/seasons/15-16/teams/"+team+"/players",
+				 		dataType: "json",
+				 		type: "GET",
+			}).done(function(response) {
+
+
+			
+			});
 		}
 	});//End Form1
 
-
+	//Table to list league standings
 	$("button.list").on("click",function(event){
 		event.preventDefault();
-		// List all Teams	
-			// $.ajax({
-			// 		headers: { 'X-Auth-Token': '35ece9681a1d4aea80ea1d8ef98fba35' },
-			// 		url: 'http://api.football-data.org/v1/soccerseasons/398/leagueTable',
-			// 		dataType: 'json',
-			// 		type: 'GET',
-			// }).done(function(response) {
-				$("#table1").DataTable({
-					"ajax" : {
-							"url" : "http://api.football-data.org/v1/soccerseasons/399/leagueTable",
-							"dataSrc" : "standing",
-							"type" : "GET",
-							"headers" : {"X-Auth-Token": "35ece9681a1d4aea80ea1d8ef98fba35"}
-					},
-					"columns": [
-						{ "data": "position" },
-            { "data": "teamName" },            
-            { "data": "playedGames" },
-            { "data": "points" }
-          ],
+		$("#table1").DataTable({
+			"ajax" : {
+				"url" : "https://sportsop-soccer-sports-open-data-v1.p.mashape.com/v1/leagues/serie-a/seasons/15-16/standings",
+				"dataSrc" : "data.standings",
+				"type" : "GET",
+				"headers" : {"X-Mashape-Key": "bilVMebejrmshNXfDlOtYvNUOxjAp1pY0GEjsn7LV5xmaJ8Dwq"}
+			},
+			"columns":[
+				{ "data": "position" },
+        { "data": "team" },            
+        { "data": "overall.macthes_played"},
+        { "data": "overall.points" }
+      ],
+      "order" : [[0,"asc"]],
+      select: true
+    });
+	});
 
-          "order" : [[0,"asc"]]
+	//Table to list topscorers in descending order
+	$("button.topscore").on("click",function(event){
+		event.preventDefault();
 
-				});
-				//var teams = response["teams"];
-				//var marketVal = response.squadMarketValue;
-				//console.log(response);
-				// for(var i=0;i<teams.length;i++)
-					// $("#table1 tbody").append("<tr><td>"+teams[i].shortName +"</td><td>" + teams[i].squadMarketValue + "</td> <td>test</td></tr>");
-			}); 					 
-		//});
+		$("#table2").DataTable({
+			"ajax": {
+				"url": "https://sportsop-soccer-sports-open-data-v1.p.mashape.com/v1/leagues/serie-a/seasons/15-16/topscorers",
+				"dataSrc" : "data.topsorers",
+				"type": "GET",
+				"headers": {"X-Mashape-Key": "bilVMebejrmshNXfDlOtYvNUOxjAp1pY0GEjsn7LV5xmaJ8Dwq"}
+			},
+			"columns": [
+				{"data":"fullname"},
+				{"data":"goals"}
+			],
+			"order" : [[1,"desc"]]
+		});
+	});
+});//End Main 					 
 
-
-});//End Main
+//var teams = response["teams"];
+//var marketVal = response.squadMarketValue;
+//console.log(response);
+// for(var i=0;i<teams.length;i++)
+// $("#table1 tbody").append("<tr><td>"+teams[i].shortName +"</td><td>" + teams[i].squadMarketValue + "</td> <td>test</td></tr>");
+		
