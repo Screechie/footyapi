0a1bb634f3fc6288b31db248e0f7e3a696e19763
sim <simeonbourne@gmail.com>
2016-02-03 20:41:02 -0400

    Add dynamic tables

--- app.js
+++ app.js
@@ -3,17 +3,30 @@ $(function(){
 
 	//Set Autocompletion for teams
 		$("#roster").autocomplete({
+			//source: ["Milan", "Juventus", "Roma", "Sampdoria", "Udinese"],
+			minLength: 3,
+			//delay: 
 			source: function(request,response){
 				$.ajax({
 					headers:{"X-Mashape-Key": "bilVMebejrmshNXfDlOtYvNUOxjAp1pY0GEjsn7LV5xmaJ8Dwq"},
 					url: "https://sportsop-soccer-sports-open-data-v1.p.mashape.com/v1/leagues/serie-a/seasons/15-16/teams",
 					dataType: "json",
-					type: "GET"
-				}).done(function(data){
-					response.data;
-				});
+					type: "GET",
+					data: {term: request.term},
+					success: function(data){
+						//use .map to transform data object into an array
+						response($.map(data.data.teams,function(value){
+												return value.name;
+										}));
+						console.log(data);
+						console.log(request.term);
+					}
+				})
 			} 
 		});
+
+
+	//Set Autocompletion for players
 
 	//Table to list a team's available players
 	$("#form").on("submit",function(event){
@@ -22,10 +35,29 @@ $(function(){
 
 		var team = $('#roster').val();//Get Team	
 
+		$("#table3_wrapper").remove();
+
 		if(team == ""){
 			alert("Please enter a team name!");//No empty team field
 		}
 		else{
+			var table3str ='<table id = "table3" class="display">'+
+				'<thead>'+
+					'<tr>'+
+						'<th>Team</th>'+
+						'<th>Name</th>'+
+						'<th>Lastname</th>'+
+						'<th>Nationality</th>'+
+						'<th>Position</th>'+
+						'<th>Jersey No.</th>'+
+				'</tr>'+
+				'</thead>'+
+				'<tbody>'+		
+				'</tbody>'+
+			'</table>';
+
+			$(table3str).appendTo("#table_3");
+
 			$('#table3').dataTable({
 				//return team squad
 				"ajax" :{
@@ -35,12 +67,18 @@ $(function(){
 				 	"type": "GET",
 				},
 				"columns" :[
-					{"data" :"team"},
-					{"data" :"name"},
-				 	{"data" :"fullname" },
-				 	{"data" :"nationality" },
-				 	{"data" : "position"},
-				 	{"data" : "number"}
+					{"data" :"team",
+					 "sClass": "center"},
+					{"data" :"name",
+					 "sClass": "center"},
+				 	{"data" :"fullname",
+				 	 "sClass": "center" },
+				 	{"data" :"nationality",
+				 	 "sClass": "center" },
+				 	{"data" : "position",
+				   "sClass": "center"},
+				 	{"data" : "number",
+				   "sClass": "center"}
 				]
 			});	
 			
@@ -77,6 +115,22 @@ $(function(){
 	//Table to list league standings
 	$("button.list").on("click",function(event){
 		event.preventDefault();
+		$("#table1_wrapper").remove();
+		var table1str = '<table id = "table1" class="display">'+
+				'<thead>'+
+					'<tr>'+
+						"<th>Position</th>"+
+						'<th>Team Name</th>'+
+						'<th>Matches Played</th>'+
+						'<th>Points</th>'+
+					'</tr>'+
+				'</thead>'+
+				'<tbody>'+
+				'</tbody>'+
+			'</table>';
+
+		$(table1str).appendTo("#table_1");
+
 		$("#table1").DataTable({
 			"ajax" : {
 				"url" : "https://sportsop-soccer-sports-open-data-v1.p.mashape.com/v1/leagues/serie-a/seasons/15-16/standings",
@@ -85,10 +139,14 @@ $(function(){
 				"headers" : {"X-Mashape-Key": "bilVMebejrmshNXfDlOtYvNUOxjAp1pY0GEjsn7LV5xmaJ8Dwq"}
 			},
 			"columns":[
-				{ "data": "position" },
-        { "data": "team" },            
-        { "data": "overall.macthes_played"},
-        { "data": "overall.points" }
+				{ "data": "position",
+					"sClass": "center"},
+        { "data": "team",
+        	"sClass": "center"},            
+        { "data": "overall.macthes_played",
+      		"sClass": "center"},
+        { "data": "overall.points",
+        	"sClass": "center"}
       ],
       "order" : [[0,"asc"]],
       select: true
@@ -98,6 +156,19 @@ $(function(){
 	//Table to list topscorers in descending order
 	$("button.topscore").on("click",function(event){
 		event.preventDefault();
+		$("#table2_wrapper").remove();
+
+		var table2 = '<table id = "table2" class="display">'+
+				'<thead>'+
+					'<tr>'+
+						'<th>Player</th>'+
+						'<th>Goals</th>'+
+				'</tr>'+
+				'</thead>'+
+				'<tbody>'+					
+				'</tbody>'+
+			'</table>';
+		$(table2).appendTo("#table_2");
 
 		$("#table2").DataTable({
 			"ajax": {
@@ -107,8 +178,10 @@ $(function(){
 				"headers": {"X-Mashape-Key": "bilVMebejrmshNXfDlOtYvNUOxjAp1pY0GEjsn7LV5xmaJ8Dwq"}
 			},
 			"columns": [
-				{"data":"fullname"},
-				{"data":"goals"}
+				{"data":"fullname",
+				 "sClass":"center"},
+				{"data":"goals",
+				 "sClass":"center"}
 			],
 			"order" : [[1,"desc"]]
 		});
